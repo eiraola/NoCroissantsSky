141ca71f371ec398a0759a140d2ea9fd389142a8
Eduardo Montero <32089193+eiraola@users.noreply.github.com>
2021-04-27 10:56:53 +0200

    Now you can build cubic worlds!

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class World : MonoBehaviour
 {
     public Material textureAtlas;
-    public static int columnHeight = 16;
+    public static int columnHeight = 2;
     public static int chunkSize = 16;
+    public static int worldSize = 2;
     public static Dictionary<string, Chunck> chunks;
 
     public static string BuildChunkName(Vector3 v) {
@@ -25,13 +26,36 @@ public class World : MonoBehaviour
             yield return null;
         }
     }
+    IEnumerator BuildWorld()
+    {
+        for (int z = 0; z < worldSize; z++)
+        {
+            for (int x = 0; x < worldSize; x++)
+            {
+                for (int y = 0; y < columnHeight; y++)
+                {
+                    Vector3 chunckPosition = new Vector3(x*chunkSize, y * chunkSize, z*chunkSize);
+                    Chunck c = new Chunck(chunckPosition, textureAtlas);
+                    c.chunck.transform.parent = this.transform;
+                    chunks.Add(c.chunck.name, c);
+                    print("xD");
+                }
+            }
+        }
+       
+        foreach (KeyValuePair<string, Chunck> c in chunks)
+        {
+            c.Value.DrawChunk();
+            yield return null;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         chunks = new Dictionary<string, Chunck>();
         this.transform.position = Vector3.zero;
         this.transform.rotation = Quaternion.identity;
-        StartCoroutine(BuildChunkColumn());
+        StartCoroutine(BuildWorld());
     }
 
     // Update is called once per frame
